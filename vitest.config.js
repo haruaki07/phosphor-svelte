833dfacf40862a5309569834b35bceffabb8dcc2
haruaki07 <misbahulk2236@gmail.com>
2023-02-06 20:37:05 +0700

    test: render test

--- vitest.config.js
+++ vitest.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-  test: {},
+  plugins: [svelte({ hot: false })],
+  test: {
+    environment: "jsdom",
+    include: ["./**/*.{test,spec}.js"],
+  },
 });
